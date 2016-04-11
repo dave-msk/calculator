cc615a8cb5bbc94c3cb993dbe0390a51791f7189
Snigdha <u5612813@uds.anu.edu.au>
2016-04-11 11:53:13 +1000

    test commit

---
+++ app/src/main/java/edu/anu/comp6442/assignment2/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.anu.comp6442.assignment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
