8eafb266a0e91b08c17baef30e1f4be2c3568faf
Snigdha <u5612813@uds.anu.edu.au>
2016-05-16 20:47:48 +1000

    test

---
+++ app/src/test/java/edu/anu/comp6442/assignment2/JUnitTest.java
@@ -0,0 +1,33 @@
+package edu.anu.comp6442.assignment2;
+
+import org.junit.Test;
+
+import parserV2.CalculatorParser;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class JUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void testParse() throws Exception {
+
+        
+    }
+    @Test
+    public void testHasCorrectFormat() throws Exception {
+        boolean actual_value = CalculatorParser.hasCorrectFormat("2+3");
+        boolean expected_value = true;
+        assertEquals("Test failed", expected_value, actual_value);
+    }
+
+    public void testIsNumeric() throws Exception {
+
+    }
+
+}
